7d1c2a2aa6547b5b29550b01dcb52288d01b9054
Fiona-T <fitracey@gmail.com>
2021-07-16 15:36:02 +0000

    test adding pandas and read csv

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
-# python code goes here
+import pandas as pd
+import numpy as np
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+print('Modules are imported.')
+
+corona_dataset_csv = pd.read_csv("Datasets/covid19_Confirmed_dataset.csv")
+# imports the dataset file
+corona_dataset_csv.head()
+# returns the first five rows of the dataset
+print(corona_dataset_csv.head())
+
+print(corona_dataset_csv.shape)
+# returns the rows and columns in a tuple e.g. (266, 104)
